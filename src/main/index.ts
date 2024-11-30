f05fa4d819d5766770eef631bf9b5bd432f1f8de
Red Rose <146128882+RED-ROSE515@users.noreply.github.com>
2024-11-30 13:46:19 -0500

    fix package.json

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,25 +1,23 @@
-import * as path from 'path';
-import { app as electronApp } from 'electron';
+import * as path from "path";
+import { app as electronApp } from "electron";
 
-import { MainConfig } from '@riboseinc/coulomb/config/main';
-import { initMain } from '@riboseinc/coulomb/app/main';
+import { MainConfig } from "@riboseinc/coulomb/config/main";
+import { initMain } from "@riboseinc/coulomb/app/main";
 
-import { ManagerOptions } from '@riboseinc/coulomb/db/isogit-yaml/main/manager';
-import { default as BackendCls } from '@riboseinc/coulomb/db/isogit-yaml/main/base';
-import { default as ModelManagerCls } from '@riboseinc/coulomb/db/isogit-yaml/main/manager';
-import { default as FSWrapper } from '@riboseinc/coulomb/db/isogit-yaml/main/yaml/directory';
+import { ManagerOptions } from "@riboseinc/coulomb/db/isogit-yaml/main/manager";
+import { default as BackendCls } from "@riboseinc/coulomb/db/isogit-yaml/main/base";
+import { default as ModelManagerCls } from "@riboseinc/coulomb/db/isogit-yaml/main/manager";
+import { default as FSWrapper } from "@riboseinc/coulomb/db/isogit-yaml/main/yaml/directory";
 
-import { default as IssueManagerCls } from './issue-manager';
+import { default as IssueManagerCls } from "./issue-manager";
 
-import { conf as appConf } from '../app';
+import { conf as appConf } from "../app";
 
-import { OBIssue } from 'models/issues';
-import { Publication } from 'models/publications';
-import { ITURecommendation } from 'models/recommendations';
+import { OBIssue } from "models/issues";
+import { Publication } from "models/publications";
+import { ITURecommendation } from "models/recommendations";
 
-
-const appDataPath = electronApp.getPath('userData');
-
+const appDataPath = electronApp.getPath("userData");
 
 export const conf: MainConfig<typeof appConf> = {
   app: appConf,
@@ -28,13 +26,13 @@ export const conf: MainConfig<typeof appConf> = {
   disableGPU: true,
 
   appDataPath: appDataPath,
-  settingsFileName: 'ituob-settings',
+  settingsFileName: "ituob-settings",
 
   databases: {
     default: {
       backend: BackendCls,
       options: {
-        workDir: path.join(appDataPath, 'ituob-data'),
+        workDir: path.join(appDataPath, "ituob-data"),
         fsWrapperClass: FSWrapper,
       },
     },
@@ -42,34 +40,44 @@ export const conf: MainConfig<typeof appConf> = {
 
   managers: {
     issues: {
-      dbName: 'default',
+      dbName: "default",
       options: {
         cls: IssueManagerCls,
-        workDir: 'issues',
-        idField: 'id',
-        metaFields: ['id', 'publication_date', 'cutoff_date', 'issn', 'languages', 'authors'],
+        workDir: "issues",
+        idField: "id",
+        metaFields: [
+          "id",
+          "publication_date",
+          "cutoff_date",
+          "issn",
+          "languages",
+          "authors",
+        ],
       } as ManagerOptions<OBIssue>,
     },
     publications: {
-      dbName: 'default',
+      dbName: "default",
       options: {
         cls: ModelManagerCls,
-        workDir: 'lists',
-        idField: 'id',
-        metaFields: ['id', 'url', 'title', 'recommendation'],
+        workDir: "lists",
+        idField: "id",
+        metaFields: ["id", "url", "title", "recommendation"],
       } as ManagerOptions<Publication>,
     },
     recommendations: {
-      dbName: 'default',
+      dbName: "default",
       options: {
         cls: ModelManagerCls,
-        workDir: 'recommendations',
-        idField: 'id',
-        metaFields: ['id', 'version', 'title'],
+        workDir: "recommendations",
+        idField: "id",
+        metaFields: ["id", "version", "title"],
       } as ManagerOptions<ITURecommendation>,
     },
   },
 };
 
-
 export const app = initMain(conf);
+
+electronApp.on("ready", () => {
+  initMain(conf);
+});
